eb484ca0eff726f3ce57bf1c181977d3d24d4774
yangzl <yangzl@git.com>
2020-01-05 19:40:19 +0800

    相关md文件

--- src/test/java/com/yinhai/AppTest.java
+++ src/test/java/com/yinhai/AppTest.java
@@ -7,12 +7,17 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
@@ -21,8 +26,6 @@ public class AppTest
     {
         assertTrue( true );
     }
-    
-    
     /*
      * 字节数组
      */
@@ -31,12 +34,31 @@ public class AppTest
         String str = "i like boom boom boom";
         byte[] bytes = str.getBytes();
         System.out.println(Arrays.toString(bytes));
-        
         byte b = -88;
         int s = b;
         System.out.println(Integer.toBinaryString(s));
         System.out.println(Integer.toBinaryString(32));
+    }
+    /**
+     * @Date: 2020/1/4
+     * @Desc: java.util.function
+     **/
+    @Test
+    public void testFunctionalInterface() {
 
+        Function<String, Integer> f = str -> str.length();
+        Function<Integer, String> f2 = num -> String.valueOf(num);
+        System.out.println(f2.andThen(f).apply(1234));
+        Stream<String> stream = Stream.of("i", "love", "this");
+        System.out.println(stream.collect(Collectors.toMap(Function.identity(),
+                String::length)));
+        String str = "123";
+        Predicate<String> predicate = Predicate.isEqual(str);
+        System.out.println(predicate.test("123"));
     }
     
+    @Test
+    public void testIntStream() {
+        System.out.println(IntStream.rangeClosed(1, 1000_00).sum());
+    }
 }
