679c64cc328cac06d7c15cf56bd773240eccf405
yangzl <yangzl@git.com>
2020-01-15 13:36:33 +0800

    juc code

---
+++ src/main/java/com/yinhai/TestDate.java
@@ -0,0 +1,43 @@
+package com.yinhai;
+
+import java.util.Date;
+
+/**
+ * @Author: yangzl
+ * @Date: 2020/1/10 15:50
+ * @Desc: .. 汇编指令
+ **/
+public class TestDate {
+
+	private int count = 0;
+
+	public static void main(String[] args) {
+		TestDate testDate = new TestDate();
+		testDate.test1();
+	}
+
+	public void test1() {
+		Date date = new Date();
+		String name1 = "wangerbei";
+		test2(date, name1);
+		System.out.println(date + name1);
+	}
+
+	public void test2(Date dateP, String name2) {
+		dateP = null;
+		name2 = "zhangsan";
+	}
+
+	public void test3() {
+		count++;
+	}
+
+	public void test4() {
+		int a = 0;
+		{
+			int b = 0;
+			b = a + 1;
+		}
+		int c = a + 1;
+	}
+}
