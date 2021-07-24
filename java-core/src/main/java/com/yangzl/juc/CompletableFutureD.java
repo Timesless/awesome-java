9a22696b170d638e260aa59742d9c4f446a0915e
yangzl <yangzl@git.com>
2021-07-24 11:03:27 +0800

    long time no see

--- java-core/src/main/java/com/yangzl/juc/CompletableFutureD.java
+++ java-core/src/main/java/com/yangzl/juc/CompletableFutureD.java
@@ -7,8 +7,72 @@ import java.util.concurrent.CompletableFuture;
  * @date 2020/1/5 15:02
  *
  *  可组合式异步回调
+ *
+ * 	1. 返回值
+ *  	1. runXxx「无返回值」
+ *  	2. supplyXxx「有返回值」
+ *
+ * 	2. 完成时回调
+ * 		whenComplete
+ * 		whenComplteAsync「再开线程执行」
+ * 		exceptionally
+ * 		handle「可以改变返回结果」
+ *
+ * 	3. 编排
+ * 		thenAccept「接收上一个线程的返回值，当前线程无返回值」
+ * 		thenAcceptAsync
+ *
+ * 		thenApply「接收上一个线程的返回值，当前线程有返回值」
+ * 		thenApplyAsync
+ *
+ * 		thenRun「不接受上一个线程的返回」
+ * 		thenRunAsync
+ *
+ * 	4. 两个任务
+ * 		both「组合的两个任务都完成」
+ * 			runAfterBoth「组合两个 future，不获取结果不返回值」
+ * 			runAfterBothAsync
+ *
+ * 			thenAcceptBoth「组合两个 future 的结果，自己无返回值」
+ * 			thenAcceptAsync
+ *
+ * 			thenCombine「组合两个 future 的结果并返回自己处理的结果」
+ *  		thenCombineAsync
+ *
+ *  	either「只要其中一个任务完成」
+ *  		runAfterEither
+ *  		runAfterEitherAsync
+ *
+ *  		acceptEither
+ *  		acceptEitherAsync
+ *
+ *  		applyToEither
+ *  		applyToEitherAsync
+ *
+ *  5. 多任务
+ *
+ *  	allOf
+ *
+ *  	anyOf
+ *
  */
 public class CompletableFutureD {
+
+	/**
+	 * handle 可以改变异常时的结果
+	 *
+	 * @throws Exception e
+	 */
+	public void handleTest() throws Exception {
+		CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+			int i = 10 / 0;
+			System.out.println("运行结果 = " + i);
+			return i;
+		}).handle((rs, throwable) -> throwable == null ? rs << 1 : 0);
+
+		System.out.println(future.get());
+	}
+
 
 	public static void main(String[] args) throws Exception {
 		// 无返回值的异步任务
@@ -20,8 +84,8 @@ public class CompletableFutureD {
 			// if (true) { throw new RuntimeException("rep"); }
 			return 200;
 		}).whenComplete((result, throwable) -> {
-			if (null != throwable) {
-				System.out.println("结果" + result);
+			if (null == throwable) {
+				System.out.println("结果 = " + result);
 			}
 		}).exceptionally(throwable -> {
 			throwable.printStackTrace();
--- java-core/src/main/java/com/yangzl/juc/ExecutorServiceD.java
+++ java-core/src/main/java/com/yangzl/juc/ExecutorServiceD.java
@@ -8,6 +8,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * @author yangzl
  * @date 2020/1/5 11:25
+ *
+ * 线程池任务执行流程
+ * 		1. 线程池创建时，准备 core 数量的核心线程
+ * 		2. core 满，随后的任务先放入阻塞队列
+ * 		3. 阻塞队列满，继续创建线程直到 max
+ * 		4. max 也满了调用 RejectExecutionHandler
+ * 		5. 在指定的的时间内，空闲的线程会从 max -> core
+ *
+ * 	new LinkedBlockingQueue<>() 容量默认是 Integer.MAX_VALUE
  */
 public class ExecutorServiceD {
 	
